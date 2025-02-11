ce9c5a14fde10c3c1cb74336935c324e0e5c1cc3
costas-leo <leonardo.costas2001@gmail.com>
2025-02-11 20:35:56 -0300

    -

--- infrastructure/repositories/mysql_llamada_repository.py
+++ infrastructure/repositories/mysql_llamada_repository.py
@@ -6,14 +6,14 @@ class MySqlLlamadaRepository(LlamadaRepository):
     def __init__(self, conexion):
         self.conexion = conexion
 
-    def obtener_llamadas_por_usuario(self, telefono, fecha_inicio, fecha_fin,id):
+    def obtener_llamadas_por_usuario(self, telefono, fecha_inicio, fecha_fin):
         conn = mysql.connector.connect(**self.conexion)
         cursor = conn.cursor(dictionary=True)
         query = """
             SELECT * FROM llamadas
             WHERE numDeSalida = %s AND fecha BETWEEN %s AND %s;
         """
-        cursor.execute(query, (telefono, fecha_inicio, fecha_fin,id))
+        cursor.execute(query, (telefono, fecha_inicio, fecha_fin))
         resultados = cursor.fetchall()
         cursor.close()
         conn.close()
